ecbc68ea5847d4c75796e5aed3637c2a965fcc50
groudjulien <groudjulien@gmail.com>
2017-03-15 20:31:32 +0100

    Optimisation
    de grosses optimisations, suppression de plusieurs boucles

--- KMeans-projet.py
+++ KMeans-projet.py
@@ -82,6 +82,9 @@ def seuil_otsu( histo ):
     return best_seuil
 
 def main( param, param_k ):
+
+    random.seed( 0 )
+
     MAX_LARGEUR = 400
     MAX_HAUTEUR = 400
 
@@ -112,7 +115,7 @@ def main( param, param_k ):
     # Le nombre de pixels de l'image
     nb_pixels = imagecolorRes.shape[0] * imagecolorRes.shape[1]
 
-    afficherImage("Image", imagecolor)
+    #afficherImage("Image", imagecolor)
 
     if (param == "OTSU"):
         imagecolor = cv2.imread('ville.png')
@@ -148,14 +151,18 @@ def main( param, param_k ):
     for i in xrange(0,K):
         groupe[i,0]=i
     #La, on fait l'attribution du reste des points de l'espace a des clusters choisis au hasard
-    for i in xrange(K,nb_pixels):
-        groupe[i,0]=random.randint(0, K-1)
+    #for i in xrange(K,nb_pixels):
+        #groupe[i,0]=random.randint(0, K-1)
+    groupe[K:nb_pixels, 0] = numpy.random.randint(0,high=K, size=(nb_pixels-K)).astype('uint8')
 
     # Initialisation des clusters dans une position aleatoire
-    for i in xrange(0, K):
-        cluster_bleu[i]=random.randint(0, 255)
-        cluster_vert[i]=random.randint(0, 255)
-        cluster_rouge[i]=random.randint(0, 255)
+    cluster_bleu[0:K] = numpy.random.randint(0, high=255, size=(K)).astype('uint8')
+    cluster_vert[0:K] = numpy.random.randint(0, high=255, size=(K)).astype('uint8')
+    cluster_rouge[0:K] = numpy.random.randint(0, high=255, size=(K)).astype('uint8')
+    #for i in xrange(0, K):
+        #cluster_bleu[i]=random.randint(0, 255)
+        #cluster_vert[i]=random.randint(0, 255)
+        #cluster_rouge[i]=random.randint(0, 255)
 
     #La, c'est a vous d'ecrire le code de la boucle principale
     #Votre code doit faire evoluer les tableaux groupe, cluster_bleu, cluster_rouge et cluster_vert
@@ -172,9 +179,11 @@ def main( param, param_k ):
 
         somme_etape += 1
 
-        old_cluster_bleu = numpy.copy(cluster_bleu)
-        old_cluster_vert = numpy.copy(cluster_vert)
-        old_cluster_rouge = numpy.copy(cluster_rouge)
+        #old_cluster_bleu = numpy.copy(cluster_bleu)
+        #old_cluster_vert = numpy.copy(cluster_vert)
+        #old_cluster_rouge = numpy.copy(cluster_rouge)
+
+        difference = 0
 
         # Calcul des positions des nouveaux points cluster
         for i in xrange(0,K):
@@ -204,9 +213,10 @@ def main( param, param_k ):
             #print "Barycentre vert : "+str(barycentre_vert)
             #print "Barycentre bleu : "+str(barycentre_bleu)
 
-            barycentre_rouge = int(barycentre_rouge/taille_du_groupe)
-            barycentre_vert = int(barycentre_vert/taille_du_groupe)
-            barycentre_bleu = int(barycentre_bleu/taille_du_groupe)
+            if (taille_du_groupe > 0):
+                barycentre_rouge = int(barycentre_rouge/taille_du_groupe)
+                barycentre_vert = int(barycentre_vert/taille_du_groupe)
+                barycentre_bleu = int(barycentre_bleu/taille_du_groupe)
 
             #print "Barycentre rouge : "+str(barycentre_rouge)
             #print "Barycentre vert : "+str(barycentre_vert)
@@ -214,16 +224,22 @@ def main( param, param_k ):
 
             # pour le moment on met les nouveaux cluster au niveau des barycentres
             if( barycentre_bleu > cluster_bleu[i] ):
+                difference += min(10, (barycentre_bleu-cluster_bleu[i]) )
                 cluster_bleu[i] = cluster_bleu[i] + min(10, (barycentre_bleu-cluster_bleu[i]) )
             else:
+                difference += min(10, (cluster_bleu[i]-barycentre_bleu))
                 cluster_bleu[i] = cluster_bleu[i] - min(10, (cluster_bleu[i]-barycentre_bleu))
             if( barycentre_vert > cluster_vert[i] ):
+                difference += min(10, (barycentre_vert-cluster_vert[i]) )
                 cluster_vert[i] = cluster_vert[i] + min(10, (barycentre_vert-cluster_vert[i]) )
             else:
+                difference += min(10, (cluster_vert[i]-barycentre_vert))
                 cluster_vert[i] = cluster_vert[i] - min(10, (cluster_vert[i]-barycentre_vert))
             if( barycentre_rouge > cluster_rouge[i] ):
+                difference += min(10, (barycentre_rouge-cluster_rouge[i]) )
                 cluster_rouge[i] = cluster_rouge[i] + min(10, (barycentre_rouge-cluster_rouge[i]) )
             else:
+                difference += min(10, (cluster_rouge[i]-barycentre_rouge))
                 cluster_rouge[i] = cluster_rouge[i] - min(10, (cluster_rouge[i]-barycentre_rouge))
 
             #print "cluster_rouge : "+str(cluster_rouge[i])
@@ -234,31 +250,39 @@ def main( param, param_k ):
         #print cluster_vert
         #print cluster_bleu
 
-        # Reaffiliation des points aux nouveaux clusters
-        valeurs_connus = dict()
-        for f in xrange(K, nb_pixels):
-            index_k_min = -1
-            if ( valeurs_connus.has_key(bleu[f,0]) and valeurs_connus[bleu[f,0]].has_key(vert[f,0]) and valeurs_connus[bleu[f,0]][vert[f,0]].has_key(rouge[f,0]) ):
-                index_k_min = valeurs_connus[bleu[f,0]][vert[f,0]][rouge[f,0]]
-            else :
-                distance_cluster = numpy.zeros(K)
-                distance_reference = -1
-                for t in xrange(0, K):
-                    distance_cluster[t] = ((bleu[f,0] - cluster_bleu[t])) ** 2 + abs(
-                        (vert[f,0] - cluster_vert[t])) ** 2 + abs((rouge[f,0] - cluster_rouge[t])) ** 2
-                    if (index_k_min == -1 or distance_cluster[t] < distance_reference):
-                        distance_reference = distance_cluster[t]
-                        index_k_min = t
-                        if( not valeurs_connus.has_key(bleu[f,0]) ):
-                            valeurs_connus[bleu[f, 0]] = dict()
-                        if( not valeurs_connus[bleu[f,0]].has_key(vert[f,0]) ):
-                            valeurs_connus[bleu[f, 0]][vert[f, 0]] = dict()
-                        valeurs_connus[bleu[f,0]][vert[f,0]][rouge[f,0]] = t
 
-            groupe[f, 0] = index_k_min
+        # Reaffiliation des points aux nouveaux clusters
+        #valeurs_connus = dict()
+        for f in xrange(0, nb_pixels):
+            index_k_min = -1
+            #if ( valeurs_connus.has_key(bleu[f,0]) and valeurs_connus[bleu[f,0]].has_key(vert[f,0]) and valeurs_connus[bleu[f,0]][vert[f,0]].has_key(rouge[f,0]) ):
+            #    index_k_min = valeurs_connus[bleu[f,0]][vert[f,0]][rouge[f,0]]
+            #else :
+            #distance_cluster = numpy.zeros(K)
+            #distance_reference = -1
+
+            distance_cluster2 = numpy.zeros(K)
+            distance_cluster2 = ((bleu[f,0] - cluster_bleu)) ** 2 + abs(
+                    (vert[f,0] - cluster_vert)) ** 2 + abs((rouge[f,0] - cluster_rouge)) ** 2
+
+            #print distance_cluster2
+
+            #for t in xrange(0, K):
+            #    distance_cluster[t] = ((bleu[f,0] - cluster_bleu[t])) ** 2 + abs(
+            #        (vert[f,0] - cluster_vert[t])) ** 2 + abs((rouge[f,0] - cluster_rouge[t])) ** 2
+            #    if (index_k_min == -1 or distance_cluster[t] < distance_reference):
+            #        distance_reference = distance_cluster[t]
+            #        index_k_min = t
+            #        if( not valeurs_connus.has_key(bleu[f,0]) ):
+            #            valeurs_connus[bleu[f, 0]] = dict()
+            #        if( not valeurs_connus[bleu[f,0]].has_key(vert[f,0]) ):
+            #           valeurs_connus[bleu[f, 0]][vert[f, 0]] = dict()
+            #           valeurs_connus[bleu[f,0]][vert[f,0]][rouge[f,0]] = t
+
+            groupe[f, 0] = numpy.argmin(distance_cluster2)
 
         # Si il n'y a pas de changement par rapport a la derniere etape : on osrt
-        if( (old_cluster_bleu==cluster_bleu).all() and (old_cluster_vert==cluster_vert).all() and (old_cluster_rouge==cluster_rouge).all() ):
+        if( difference == 0 ):
             there_are_no_modification = 1
 
 
@@ -275,6 +299,7 @@ def main( param, param_k ):
 
     #On change chaque pixel de l'image selon le cluster auquel il appartient
     #Il prendre comme nouvelle valeur la position moyenne du cluster
+    #imagecolorRes[:, :] = [ (cluster_bleu[int(groupe[:,:])]) , (cluster_vert[int(groupe[:,:])]), (cluster_rouge[int(groupe[:,:])]) ]
     for i in xrange(0, imagecolorRes.shape[0]):
         for j in xrange(0, imagecolorRes.shape[1]):
             imagecolorRes[i,j,0] = (cluster_bleu[int(groupe[i,j])])
@@ -295,7 +320,7 @@ def main( param, param_k ):
 
 
 if __name__ == "__main__":
-    main( "normal",3 )
-    main( "HSV",3 )
-    main( "LAB",3 )
+    main( "normal",32 )
+    #main( "HSV",3 )
+    #main( "LAB",3 )
     #main( "OTSU",2 )
